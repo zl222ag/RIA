a9d38756d0f2577a743f662efc2de6713b114320
Zlatko Ladan <zl222ag@student.lnu.se>
2013-12-04 17:59:51 +0100

    Winter cleaning, is now some what playable.
    + Made a couple of comments nicer.
    + Aded usage of Jade.
    + Is partly
    playable now.
    + Word lists are in a separate folder.
    + Made object names nicer.

--- script/view.js
+++ script/view.js
@@ -1,39 +1,54 @@
-//© Zlatko Ladan 2013
+// © Zlatko Ladan 2013
 
 // ## The View
-define(["backbone"], function (Backbone) {
+define(["backbone", "jade"], function (Backbone, Jade) {
 	"use strict";
 	return Backbone.View.extend({
 		el: 'body',
-		headerTagName: '<h1 />',
-		textTagName: '<p />',
-		headerClass: "text-container text-center",
-		textClass: "text-center",
+		headerTagFormat: 'h1.text-container.text-center #{text}',
+		textTagFormat: 'p.text-center',
+		inputTextTagFormat: 'p.text-center',
 		textTag: null,
+		inputTextTag: null,
 
 		text: function () {
-			return this.model.text;
+			return this.model.HEADER_TEXT;
 		},
 
 		keyPress: function () {
 			return this.model.keyPress(this.change());
 		},
 
+		onLoadLang: function () {
+			var that = this;
+
+			return function () {
+				that.textTag.text(that.model.getCurrentWord());
+			};
+		},
+
 		change: function () {
 			var that = this;
+
 			return function (a_char) {
-				that.textTag.text(that.textTag.text() + a_char);
+				that.inputTextTag.text(that.inputTextTag.text() + a_char);
 			};
 		},
 
 		render: function () {
+			var jHeader = null, jText = null, jInputText = null;
+
+			jHeader = Jade.compile(this.headerTagFormat);
+			jText = Jade.compile(this.textTagFormat);
+			jInputText = Jade.compile(this.inputTextTagFormat);
+
 			// Adds to the body
 			$(this.el).append(
 				// Adds the header with text to the body
-				$(this.headerTagName).text(this.text()).addClass(this.headerClass)
+				jHeader({text: this.text()})
 			);
 
-			this.textTag = $(this.textTagName).addClass(this.textClass);
+			this.textTag = $(jText());
 
 			// Adds to the body
 			$(this.el).append(
@@ -41,7 +56,16 @@ define(["backbone"], function (Backbone) {
 				this.textTag
 			);
 
+			this.inputTextTag = $(jInputText());
+
+			// Adds to the body
+			$(this.el).append(
+				// Adds the input text element to the body
+				this.inputTextTag
+			);
+
 			$(document).keypress(this.keyPress());
+			this.model.getWordList(this.onLoadLang());
 			return this;
 		}
 	});
